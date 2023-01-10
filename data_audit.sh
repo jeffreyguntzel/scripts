d9f1ba6de95cb1bdf5d70b23b68bd6b8a2bb3490
Jeffrey Guntzel <95197327+jeffreyguntzel@users.noreply.github.com>
2023-01-10 17:05:29 -0600

    Initial commit of data_audit script.

---
+++ data_audit.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+ 
+usage () { echo "${0##*/} inputfile outputfile"; exit 1; }
+ 
+(($#==2)) || usage
+ 
+INPUTFILE="$1"
+OUTPUTFILE="$2"
+ 
+cat <<EOF >$OUTPUTFILE
+$(date "+Generated on %m/%d/%y at %H:%M:%S")
+ 
+DATA AUDIT: $1
+ 
+------------
+COLUMN NAMES
+------------
+ 
+$(csvcut -n $INPUTFILE)
+ 
+---------------------------------------
+FIRST TEN ROWS OF FIRST FIVE COLUMNS
+---------------------------------------
+ 
+$(csvcut -c 1,2,3,4,5 $INPUTFILE | head -n 10)
+ 
+------------
+COLUMN STATS
+------------
+ 
+$(csvcut $INPUTFILE | csvstat )
+ 
+---END AUDIT
+
+EOF
+ 
+echo "Audited!"
