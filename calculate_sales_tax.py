89f7cd68eb6277adf9604aaba05592efc6f4f8af
Jeffrey Guntzel <95197327+jeffreyguntzel@users.noreply.github.com>
2023-01-10 16:37:15 -0600

    Update comments and sales tax amount.

--- calculate_sales_tax.py
+++ calculate_sales_tax.py
@@ -1,13 +1,14 @@
 import sys
 import pyperclip
 
-# Get amount from command line argument
+# Get pre-tax dollar amount from command line argument
 amount = float(sys.argv[1])
 
-# Calculate total
-total = round(amount + (amount * .07525),2)
+# Calculate total based on 8.025% sales tax
+total = round(amount + (amount * .0825),2)
 
 # Place total on clipboard
 pyperclip.copy(total)
 
+# Let user know what was copied to the clipboard
 print(f"Total copied to clipboard: {total}")
